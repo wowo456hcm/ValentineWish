0a082796bcdf4ec4a2639e09fe1ba50be386eb7e
wowo456hcm <109791959+wowo456hcm@users.noreply.github.com>
2025-02-13 22:03:18 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -105,20 +105,20 @@ const createAnimationTimeline = () => {
             },
             "+=0.7"
         )
-        .from(".idea-1", 1, ideaTextTrans)
-        .to(".idea-1", 1, ideaTextTransLeave, "+=1.5")
-        .from(".idea-2", 1, ideaTextTrans)
-        .to(".idea-2", 1, ideaTextTransLeave, "+=1.5")
-        .from(".idea-3", 1, ideaTextTrans)
-        .to(".idea-3 strong", 1, {
+        .from(".idea-1", 0.5, ideaTextTrans)
+        .to(".idea-1", 0.5, ideaTextTransLeave, "+=1.5")
+        .from(".idea-2", 0.5, ideaTextTrans)
+        .to(".idea-2", 0.5, ideaTextTransLeave, "+=1.5")
+        .from(".idea-3", 0.5, ideaTextTrans)
+        .to(".idea-3 strong", 0.5, {
             scale: 1.2,
             x: 10,
             backgroundColor: "rgb(21, 161, 237)",
             color: "#fff",
         })
-        .to(".idea-3", 1, ideaTextTransLeave, "+=1.5")
-        .from(".idea-4", 1, ideaTextTrans)
-        .to(".idea-4", 1, ideaTextTransLeave, "+=1.5")
+        .to(".idea-3", 0.5, ideaTextTransLeave, "+=1.5")
+        .from(".idea-4", 0.5, ideaTextTrans)
+        .to(".idea-4",0.51, ideaTextTransLeave, "+=1.5")
         .from(
             ".idea-5",
             1,
